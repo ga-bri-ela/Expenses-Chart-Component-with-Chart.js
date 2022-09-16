4a4637fc2de507f43a004c012d83a95be875bdf5
Gabriela Aarnio <gtaricano@gmail.com>
2022-09-16 17:25:32 +0300

    -
    Now the data is retrieved with async fetch().
    
    Refactored the Chart.js.

--- script.js
+++ script.js
@@ -1,94 +1,101 @@
-import dataJSON from '/data.json' assert {type: 'json'};
-
-/*
 async function fetchData() {
     const url= 'data.json';
     const response = await fetch(url);
-    const dataPoints = await response.json();
-    return dataPoints;
-}
-
-fetchData().then(datapoints => {
-    const weekData = datapoints.map(
-        function(index){
-            return index;
-        });
-    console.log(index)
-    });
-*/
-
+    const datapoints = await response.json();
+    return datapoints;
+};
 
 const d = new Date();
 let today = d.getDay();
 let adjustedDay = today - 1;
 
-
 const daysHover = ['hsl(10, 79%, 85%)', 'hsl(10, 79%, 85%)', 'hsl(10, 79%, 85%)','hsl(10, 79%, 85%)', 'hsl(10, 79%, 85%)','hsl(10, 79%, 85%)'];
 daysHover.splice(adjustedDay, 0, 'hsl(186, 34%, 85%)');
 
+let days = [];
+let amount = [];
 
-window.onload = function () {
-    const ctx = document.getElementById('myChart').getContext('2d');
+fetchData().then(datapoints => {
+    let dataTotal = datapoints;
+    days = datapoints.map(
+        function(index){
+            return index.day;
+        });
+
+    amount = datapoints.map(
+        function(index){
+            return index.amount;
+    })
+
     let orangeBar = 'hsl(10, 79%, 65%)';
     let blueBar = 'hsl(186, 34%, 60%)';
-
-
-  
-    const myChart = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: dataJSON.map((element) => element.day),
+    const labels = days;
+    
+      const data = {
+        labels: labels,
         datasets: [{
-            data: dataJSON.map((element) => element.amount),
-            backgroundColor: (color) => {
-              let backgroundColor = color.dataIndex === adjustedDay ? blueBar : orangeBar;
-              return backgroundColor;
-            },
-            hoverBackgroundColor: (daysHover),
-            borderSkipped: false,
-            borderRadius: 5,
-        }],
-      },
-      options: {
-        responsive: true,
-        plugins: {
-            legend: {
-                display: false
-            },
-            tooltip:{
-                backgroundColor: 'hsl(25, 47%, 15%)', 
-                displayColors: false,
-                cornerRadius: 5,
-                padding: 10,
-                caretSize: 0,
-                callbacks: {
-                    title : (function () {
-                        return null;
-                    }),
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: amount,
+          backgroundColor: (color) => {
+            let backgroundColor = color.dataIndex === adjustedDay ? blueBar : orangeBar;
+            return backgroundColor;
+          },
+          hoverBackgroundColor: (daysHover),
+          borderSkipped: false,
+          borderRadius: 5,          
+        }]
+      };
+    
+      const config = {
+        type: 'bar',
+        data: data,
+        options: { 
+            responsive: true,
+            scales: {
+                x: {
+                    title:{
+                        display: true,
+                    },
+                    grid: {
+                        display: false,
+                        borderColor: 'white',
+                    },
                 },
-            }
-        },
-        scales: {
-            x: {
-                title:{
-                    display: true,
-                },
-                grid: {
-                    display: false,
-                    borderColor: 'white',
-                },
-            },
-            y : {
-                beginAtZero: true,
-                grid: {
-                    display: false,
-                    borderColor: 'white',
-                },
-                ticks: {
-                    display: false,
+                y : {
+                    beginAtZero: true,
+                    grid: {
+                        display: false,
+                        borderColor: 'white',
+                    },
+                    ticks: {
+                        display: false,
+                    },
                 },
             },
-        },
-      },
-    });
-  };
+            plugins: {
+                legend: {
+                    display: false
+                },                
+                tooltip:{
+                    backgroundColor: 'hsl(25, 47%, 15%)', 
+                    displayColors: false,
+                    cornerRadius: 5,
+                    padding: 10,
+                    caretSize: 0,
+                    callbacks: {
+                        title : (function () {
+                            return null;
+                        }),
+                    },
+                },
+            },
+            
+        }
+      };
+    
+      const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+      );
+});   
